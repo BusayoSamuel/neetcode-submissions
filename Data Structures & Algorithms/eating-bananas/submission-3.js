2cccbfddf6505684cd65a1181d47d974ba5d582f
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2026-04-19 09:59:40 +0100

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        function calcTime(k){
+            let t = 0
+            for(var pile of piles){
+                t += Math.ceil(pile/k)
+            }
+            return t
+        }
+
+        let l = 1
+        let r = Math.max(...piles)
+        var res = r
+
+        while (l <= r){
+            let m = Math.floor((l + r)/2)
+
+            if (calcTime(m) > h){
+                l = m + 1
+            }else{
+                res = m
+                r = m - 1
+            }
+        }
+        return res
+
+    }
+}
