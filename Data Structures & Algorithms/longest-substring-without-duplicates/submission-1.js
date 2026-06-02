048b98f8a8c15d59d13bcbda280636799b3b1de9
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2026-06-02 09:01:48 +0100

    Add: longest-substring-without-duplicates - submission-1

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        const hashset = new Set();
+        var res = 0;
+        var l = 0;
+
+        for(let r = 0; r < s.length; r++){
+            while(l <= r && hashset.has(s[r])){
+                hashset.delete(s[l]);
+                l += 1;
+            }
+
+            hashset.add(s[r]);
+            res = Math.max(res, r - l + 1);
+        }
+
+        return res;
+    }
+}
