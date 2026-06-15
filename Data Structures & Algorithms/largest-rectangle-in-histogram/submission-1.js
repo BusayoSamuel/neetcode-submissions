d43d8e121ff32daaca015fa5f404803a1330409a
Oluwabusayomi Samuel <busayosalisu@gmail.com>
2026-06-15 11:18:20 +0100

    Add: largest-rectangle-in-histogram - submission-1

---
+++ Data Structures & Algorithms/largest-rectangle-in-histogram/submission-1.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    largestRectangleArea(heights) {
+        const stack = []
+        var res = 0
+
+        for(let i = 0; i < heights.length; i++){
+            let start = i
+            while(stack.length && stack[stack.length-1][1] > heights[i]){
+                var [index, height] = stack.pop()
+                res = Math.max(res, height * (i - index))
+                start = index
+            }
+            stack.push([start, heights[i]])
+        }
+
+        const i = heights.length
+        while(stack.length){
+            var [index, height] = stack.pop()
+            res = Math.max(res, height * (i - index))  
+        }
+
+        return res
+    }
+}
